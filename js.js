187e6e399da69de704aab1dbe22c72e83377d8aa
Goose <lbeutlich@epacube.com>
2017-12-29 11:26:34 -0500

    first commit

---
+++ js.js
@@ -0,0 +1,24 @@
+(function() {
+ //  console.log("test");
+ //  var p = document.createElement("p");
+ //  var node = document.createTextNode("This is a test");
+ //  p.appendChild(node);
+ // document.body.appendChild(p);
+  var imgs = document.getElementsByTagName("img");
+  var src = "";
+  var ajax = new XMLHttpRequest();
+  ajax.onload = function() {
+     var res = this.response;
+     src = res.match(/src=(".+")/g);
+     for (var j = 0; j < src.length; j++) {
+       src[j] = src[j].replace(/(src=|")/g, "");
+     }
+     console.log(src);
+     for (var i = 0; i < imgs.length; i++) {
+       imgs[i].src = src[i];
+     }
+
+   }
+  ajax.open("GET", "http://thecatapi.com/api/images/get?format=html&api_key=MjYwNjY2&results_per_page=" + imgs.length);
+  ajax.send();
+})();
